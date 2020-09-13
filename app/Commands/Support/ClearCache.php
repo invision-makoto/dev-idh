078c18a9d168ff07471d00ffccafc6f869b6dcaf
Makoto <makoto+github@taiga.sh>
2020-09-13 04:03:42 -0400

    ClearCache command

---
+++ app/Commands/Support/ClearCache.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Commands\Support;
+
+use App\Invision\Invision;
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class ClearCache extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'support:cache';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Manually clear all IPS cache files';
+
+    /**
+     * @var \App\Invision\Invision
+     */
+    protected $ips;
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->ips = app( Invision::class );
+
+        $this->task( 'Clearing javascript files', function () {
+            $this->ips->clearJsFiles();
+        } );
+
+        $this->task( 'Clearing compiled CSS files', function () {
+            $this->ips->clearJsFiles();
+        } );
+
+        $this->task( 'Clearing cache files', function () {
+            $this->ips->clearCache();
+            \IPS\Member::clearCreateMenu();
+        } );
+
+        $this->task( 'Clearing data store', function () {
+            $this->ips->clearDataStore();
+        } );
+
+        $this->task( 'Clearing \IPS\Helpers\Wizard AdminCP sessions', function () {
+            $this->ips->clearWizardSessions();
+        } );
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
